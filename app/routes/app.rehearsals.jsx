369318a8fb152ae22fcc54acc34a2db299bd901f
Amy Mullin <IDA@macbookpro.mynetworksettings.com>
2026-09-04 13:22:01 -0400

    Remove Polaris from Rehearsals pages

--- app/routes/app.rehearsals.jsx
+++ app/routes/app.rehearsals.jsx
@@ -1,30 +1,17 @@
-import { Page, Card, BlockStack, Text } from "@shopify/polaris";
-
 export default function RehearsalsPage() {
   return (
-    <Page title="Rehearsals">
-      <BlockStack gap="400">
-        <Card>
-          <BlockStack gap="200">
-            <Text as="h2" variant="headingMd">
-              Welcome to the Rehearsals Module
-            </Text>
+    <>
+      <h1>Rehearsals</h1>
 
-            <Text as="p" variant="bodyMd">
-              This module will manage:
-            </Text>
+      <p>Welcome to the Rehearsals module.</p>
 
-            <ul>
-              <li>Solo & Duet Entries</li>
-              <li>Teacher Assignments</li>
-              <li>Teacher Availability</li>
-              <li>Parent Availability</li>
-              <li>Rehearsal Scheduling</li>
-              <li>Apple Calendar Integration</li>
-            </ul>
-          </BlockStack>
-        </Card>
-      </BlockStack>
-    </Page>
+      <ul>
+        <li>Solo & Duet Entries</li>
+        <li>Teacher Assignments</li>
+        <li>Teacher Availability</li>
+        <li>Parent Availability</li>
+        <li>Scheduling</li>
+      </ul>
+    </>
   );
 }
