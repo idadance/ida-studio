6e2d82e33cc654fb93f04d37c426da14710c5cfd
Amy Mullin <IDA@macbookpro.mynetworksettings.com>
2026-09-05 08:25:28 -0400

    Load teacher on edit page

--- app/routes/app.rehearsals.edit-teacher.jsx
+++ app/routes/app.rehearsals.edit-teacher.jsx
@@ -1,13 +1,51 @@
-import { useSearchParams } from "react-router";
+import {
+  useLoaderData,
+} from "react-router";
+
+import {
+  authenticate,
+} from "../shopify.server";
+
+import {
+  getTeacher,
+} from "../services/teacher.server";
+
+export const loader = async ({
+  request,
+}) => {
+  await authenticate.admin(request);
+
+  const url = new URL(request.url);
+
+  const id =
+    url.searchParams.get("id");
+
+  return {
+    teacher: await getTeacher(id),
+  };
+};
 
 export default function EditTeacherPage() {
-  const [searchParams] = useSearchParams();
-
-  const id = searchParams.get("id");
+  const { teacher } =
+    useLoaderData();
 
   return (
     <s-page heading="Edit Teacher">
-      <p>Teacher ID: {id}</p>
+      <h2>
+        {teacher.firstName}{" "}
+        {teacher.lastName}
+      </h2>
+
+      <p>
+        Maximum Solo/Duets:{" "}
+        {teacher.maxSoloDuets}
+      </p>
+
+      <p>
+        {teacher.active
+          ? "✅ Active"
+          : "⚪ Inactive"}
+      </p>
     </s-page>
   );
 }
